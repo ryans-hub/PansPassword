615bfee08b55e463a7f8dcbcb0634dad53562bfc
PanMan <ryanispan@gmail.com>
2024-01-13 18:15:13 -0700

    updates

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Event listener for generate button, prompts
+generateBtn.addEventListener("click", promptMe);
+
+//function to prompt user 
+function promptMe() {
+  let prompt1 = prompt("Length of password? (8-128)");
+  let prompt2 = prompt("Type of characters?");
+}
+
+var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var numeric = "0123456789".split("");
+var special = "!\"#$%&'()*+,-./:;<=>?@[]^_`{|\\}~".split("");
+

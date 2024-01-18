021205d501244bff243af2942e8bb6770db195b6
PanMan <ryanispan@gmail.com>
2024-01-17 21:17:47 -0700

    updated JS file, placed code inside while loop

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 //Event listener for generate button, prompts 
-generateBtn.addEventListener("click", promptMe);
+generateBtn.addEventListener("click", generatePassword);
 
 //Character Arrays
 var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
@@ -22,42 +22,47 @@ var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var numeric = "0123456789".split("");
 var special = "!\"#$%&'()*+,-./:;<=>?@[]^_`{|\\}~".split("");
 
-//Blank array that will hold the generated password characters
-var newPassword = [];
-
 //Confirming what user wants password to contain
-function promptMe() {
+function generatePassword() {
   var passwordLength = prompt("Length of password? (8-128)");
   var isLowerCase = confirm("Do you want lowercase letters?");
   var isUpperCase = confirm("Do you want uppercase letters?");
   var isNumeric = confirm("Do you want numbers?");
   var isSpecial = confirm("Do you want special characters?");
 
-  if (passwordLength === )
+  if (passwordLength >= 8 && passwordLength <=128 ) {
 
-  if (isLowerCase) {
-    var RandomCharacter = Math.floor(Math.random()*26);
-    newPassword.push(lowerCase[RandomCharacter]);
-    
+    var inputLength = parseInt(passwordLength);
+
+    var passwordAr = [];
+
   }
 
-  if (isUpperCase) {
-    var RandomCharacter = Math.floor(Math.random()*26);
-    newPassword.push(upperCase[RandomCharacter]);
+  while (passwordAr.length < inputLength) {
+
+    if (isLowerCase) {
+      var RandomCharacter = Math.floor(Math.random()*26);
+      passwordAr.push(lowerCase[RandomCharacter]);
+      
+    }
+  
+    if (isUpperCase) {
+      var RandomCharacter = Math.floor(Math.random()*26);
+      passwordAr.push(upperCase[RandomCharacter]);
+    }
+  
+    if (isNumeric) {
+      var RandomCharacter = Math.floor(Math.random()*10);
+      passwordAr.push(numeric[RandomCharacter]);
+    }
+  
+    if (isSpecial) {
+      var RandomCharacter = Math.floor(Math.random()*32);
+      passwordAr.push(special[RandomCharacter]);
+    }
+  
+
   }
-
-  if (isNumeric) {
-    var RandomCharacter = Math.floor(Math.random()*10);
-    newPassword.push(numeric[RandomCharacter]);
-  }
-
-  if (isSpecial) {
-    var RandomCharacter = Math.floor(Math.random()*32);
-    newPassword.push(special[RandomCharacter]);
-  }
-
-  console.log(newPassword);
-
 
 }
 

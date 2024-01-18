4bcc1221f7712a89fc238d9614033b686029870c
PanMan <ryanispan@gmail.com>
2024-01-17 20:19:14 -0700

    updating JS file, all new code

--- script.js
+++ script.js
@@ -13,17 +13,53 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//Event listener for generate button, prompts
+//Event listener for generate button, prompts 
 generateBtn.addEventListener("click", promptMe);
 
-//function to prompt user 
-function promptMe() {
-  let prompt1 = prompt("Length of password? (8-128)");
-  let prompt2 = prompt("Type of characters?");
-}
-
+//Character Arrays
 var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var numeric = "0123456789".split("");
 var special = "!\"#$%&'()*+,-./:;<=>?@[]^_`{|\\}~".split("");
+
+//Blank array that will hold the generated password characters
+var newPassword = [];
+
+//Confirming what user wants password to contain
+function promptMe() {
+  var passwordLength = prompt("Length of password? (8-128)");
+  var isLowerCase = confirm("Do you want lowercase letters?");
+  var isUpperCase = confirm("Do you want uppercase letters?");
+  var isNumeric = confirm("Do you want numbers?");
+  var isSpecial = confirm("Do you want special characters?");
+
+  if (passwordLength === )
+
+  if (isLowerCase) {
+    var RandomCharacter = Math.floor(Math.random()*26);
+    newPassword.push(lowerCase[RandomCharacter]);
+    
+  }
+
+  if (isUpperCase) {
+    var RandomCharacter = Math.floor(Math.random()*26);
+    newPassword.push(upperCase[RandomCharacter]);
+  }
+
+  if (isNumeric) {
+    var RandomCharacter = Math.floor(Math.random()*10);
+    newPassword.push(numeric[RandomCharacter]);
+  }
+
+  if (isSpecial) {
+    var RandomCharacter = Math.floor(Math.random()*32);
+    newPassword.push(special[RandomCharacter]);
+  }
+
+  console.log(newPassword);
+
+
+}
+
+
 
